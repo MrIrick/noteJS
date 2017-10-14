e411452118e3b951ea4626ff2aafbeb19017525e
hypin <haiping@staff.weibo.com>
2017-10-14 15:58:57 +0800

    add moment module

--- test2.js
+++ test2.js
@@ -1,6 +1,6 @@
 var path = require('path')
 var http = requre('http')
-
+var moment = require('moment')
 http.createServer(function(req, res){
 	console.log('hello world')
 	res.send('<html><head><title>海平</title><head><body><h1>HelloWorld</h1></body></html>')
